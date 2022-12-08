e46f6179f66dcfff636244566a4f90eefa7829cc
M.Ferhat KARAGÖZ <mkaragoz4@gmail.com>
2022-12-08 16:20:24 +0100

    firstcommit

---
+++ Java1Week1HW1/src/assignment/Question1.java
@@ -0,0 +1,87 @@
+package assignment;
+
+public class Question1 {
+	
+	public static void main(String[] args) {
+		
+		
+		//byte
+		
+		byte a=12;
+		System.out.println(a);
+		byte b=127;
+		System.out.println(b);
+		byte c=34;
+		System.out.println(c);
+		
+		//short
+		
+		short x=45;
+		System.out.println(x);
+		short k=678;
+		System.out.println(k);
+		short l=333;
+		System.out.println(l);
+		
+		
+		//int
+		
+		int örnek=5555;
+		System.out.println(örnek);
+		int örnek2=333;
+		System.out.println(örnek2);
+		int örnek3=4545;
+		System.out.println(örnek3);
+		
+		//long
+		
+		long j=2323;
+		System.out.println(j);
+		long w=1234535;
+		System.out.println(w);
+		long n=232323;
+		System.out.println(n);
+		
+		//float
+		
+		float ex1=3.45f;
+		System.out.println(ex1);
+		float ex2=3.25f;
+		System.out.println(ex2);
+		float ex3=2.4f;
+		System.out.println(ex3);
+		
+		//double
+		
+		double aa1=2.3454545;
+		System.out.println(aa1);
+		double aa2=3.454545;
+		System.out.println(aa2);
+		double aa3=3.455665;
+		System.out.println(aa3);
+		
+		//char
+		
+		char ch='K';
+		System.out.println(ch);
+		char ch1='\'';
+		System.out.println(ch1);
+		char ch3='k';
+		System.out.println(ch3);
+		
+		//boolean
+		
+		boolean wr=false;
+		System.out.println(wr);
+		boolean tr=true;
+		System.out.println(tr);
+		boolean kk=false;
+		System.out.println(kk);
+		
+		
+		
+		
+		
+	}
+
+}
---
+++ Java1Week1HW1/src/assignment/Question2.java
@@ -0,0 +1,49 @@
+package assignment;
+
+public class Question2 {
+	
+	public static void main(String[]args) {
+		
+		//MAX VALUES OF PRİMİTİVE DATA TYPES
+		
+		//byte
+		
+		byte max=Byte.MAX_VALUE;
+		System.out.println(max);
+		byte min=Byte.MIN_VALUE;
+		System.out.println(min);
+		
+		//short
+		
+		short max_deger=Short.MAX_VALUE;
+		System.out.println(max);
+		short min_deger=Short.MIN_VALUE;
+		System.out.println(min_deger);
+		
+		//long
+		
+		System.out.println(Long.MAX_VALUE);
+		System.out.println(Long.MIN_VALUE);
+		
+		//Int
+				
+		System.out.println(Integer.MAX_VALUE);
+		int minimum=Integer.MIN_VALUE;
+		System.out.println(minimum);
+		
+		//float
+		
+		System.out.println(Float.MAX_VALUE);
+		System.out.println(Float.MIN_VALUE);
+		
+		//double
+		
+		System.out.println(Double.MAX_VALUE);
+		System.out.println(Double.MIN_VALUE);
+		
+	
+		
+		
+	}
+
+}
